fcea644d144a0b27cbc622cc0dfdaa53f203656d
inmaugarc <inmaugarc@yahoo.es>
2021-01-04 18:15:26 +0100

    Update process_data.py

--- data/process_data.py
+++ data/process_data.py
@@ -1,19 +1,103 @@
+"""
+This script loads 3 files:
+    - the messages file in a csv format
+    - the categories file in a csv format
+    - the db file
+Then it cleans and prepares data for the next phases of this project
+And it saves the cleaned data on a SQLite destination database
+
+Script execution syntax:
+    python process_data.py <path_to_messages_file.csv> <path_to_categories_file.csv> <path_to_SQLite_response_database.db>
+Sample script execution:
+    python process_data.py disaster_messages.csv disaster_categories.csv disaster_response.db
+
+"""
+# Import the libraries we are going to use
 import sys
+import pandas as pd
+from sqlalchemy import create_engine
 
 
 def load_data(messages_filepath, categories_filepath):
-    pass
+    """
+    Load messages_file and categories_file
+    Args:
+        messages_filepath: csv file with the disaster messages
+        categories_filepath: csv file with the categories for disaster messages
+    Returns: a file that is merge of the two input files
+    """
+
+    df_mes = pd.read_csv(messages_filepath)
+    df_cat = pd.read_csv(categories_filepath)
+    df = df_mes.merge(df_cat,how="inner", on=["id"])
+
+    return df
 
 
 def clean_data(df):
-    pass
+        """
+        This script cleans data
+        Args:
+            pandas-dataframe file: it contains the data to clean
+        Returns: a dataframe file with clean data
+        """
+    # Let's split the categories into separated columns
+    # create a dataframe of the 36 individual category columns
+    categories = df['categories'].str.split(pat=";",expand=True)
+
+    # select the first row of the categories dataframe
+    row = categories.iloc[[0]]
+
+    # use this row to extract a list of new column names for categories.
+    category_colnames = [category_name.split('-')[0] for category_name in row.values[0]]
+    # rename the columns of `categories`
+    categories.columns = category_colnames
+
+    # convert category values to just numbers 0 or 1
+    for column in categories:
+        # set each value to be the last character of the string
+        categories[column] = categories[column].astype(str).str[-1:]
+        # convert column from string to numeric
+        categories[column] = categories[column].astype(int)
+
+    # replace categories column in df with new category columns
+    # drop the original categories column from `df`
+    df.drop('categories',axis=1,inplace=True)
+    # concatenate the original dataframe with the new `categories` dataframe
+    df = pd.concat([df,categories],join="inner",axis=1)
+
+    # drop duplicates
+    df.drop_duplicates(inplace=True);
+
+    return df
 
 
 def save_data(df, database_filename):
-    pass  
+        """
+        This script saves cleaned data in to a database file
+        Args:
+            pandas-dataframe file: it contains the already cleaned data
+            database-filename: path to the database file where we will save data
+        Returns: ?
+        """
+        # save the clean dataset into an sqlite database
+        engine = create_engine('sqlite:///'+ database_filename')
+        df.to_sql('InsertTableName', engine, index=False)
 
 
 def main():
+        """
+        Main function that runs all data processing, that is:
+        1. Load messages and categories files
+        2. Clean data
+        3. Save data into a SQLite Database
+
+        Args:
+            pandas-dataframe file: it contains the already cleaned data
+            database-filename: path to the database file where we will save data
+        Returns:
+        """
+
     if len(sys.argv) == 4:
 
         messages_filepath, categories_filepath, database_filepath = sys.argv[1:]
@@ -24,12 +108,12 @@ def main():
 
         print('Cleaning data...')
         df = clean_data(df)
-        
+
         print('Saving data...\n    DATABASE: {}'.format(database_filepath))
         save_data(df, database_filepath)
-        
+
         print('Cleaned data saved to database!')
-    
+
     else:
         print('Please provide the filepaths of the messages and categories '\
               'datasets as the first and second argument respectively, as '\
